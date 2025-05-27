bccfbff8c2a162d6ea3e2c1748d5e7cce2e774b8
hjk1348 <gudwls1815@naver.com>
2025-05-28 07:33:00 +0900

    [#4]feat: 학적 조회

---
+++ app/src/main/java/com/example/hallymone/CreditActivity.kt
@@ -0,0 +1,20 @@
+package com.example.hallymone
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CreditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_credit)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
